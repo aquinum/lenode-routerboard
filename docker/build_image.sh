106a0e5b5923c7fb6baf14392f4e7f4914dcce4a
Arnaud de Mouhy <arnaud.demouhy@akerbis.com>
2016-12-19 17:25:14 +0100

    Docker image build optimizations

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -4,13 +4,14 @@ useradd routerboard
 
 apt-get update
 apt-get install --no-install-recommends -y \
-    npm
+    npm \
+    git
 
 php /composer-setup.php --install-dir=/usr/bin --filename=composer
 rm /composer-setup.php
 
 cd /var/www/html
-composer install
+composer install --prefer-dist --no-dev --optimize-autoloader
 npm install
 
 chown -R routerboard: /var/www/html/*
